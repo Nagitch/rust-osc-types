b2eee5a060ff4b82ad5a706e90c6b44955b0297d
Nagitch <kusanagi.y@mujina-prod.com>
2025-10-10 17:37:43 +0900

    Add unit tests covering OSC data types

--- osc-types-core/src/lib.rs
+++ osc-types-core/src/lib.rs
@@ -70,3 +70,88 @@ pub enum OscPacket {
     Message(OscMessage),
     Bundle(OscBundle),
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloc::vec;
+
+    #[test]
+    fn address_plausibility_requires_leading_slash() {
+        let plausible = Address("/synth/volume".into());
+        let implausible = Address("synth/volume".into());
+
+        assert!(plausible.is_plausible());
+        assert!(!implausible.is_plausible());
+    }
+
+    #[test]
+    fn osc_message_supports_core_types() {
+        let message = OscMessage {
+            addr: Address("/core".into()),
+            args: vec![
+                OscType::Int(42),
+                OscType::Float(0.5),
+                OscType::String("hi".into()),
+                OscType::Blob(vec![1, 2, 3, 4]),
+            ],
+        };
+
+        assert_eq!(message.addr, Address("/core".into()));
+        assert_eq!(message.args.len(), 4);
+        assert_eq!(message.args[0], OscType::Int(42));
+        assert_eq!(message.args[1], OscType::Float(0.5));
+        assert_eq!(message.args[2], OscType::String("hi".into()));
+        assert_eq!(message.args[3], OscType::Blob(vec![1, 2, 3, 4]));
+    }
+
+    #[test]
+    fn osc_bundle_supports_nested_packets() {
+        let inner_message = OscMessage {
+            addr: Address("/inner".into()),
+            args: vec![OscType::Int(1)],
+        };
+        let packet = OscPacket::Message(inner_message.clone());
+        let bundle = OscBundle {
+            timetag: TimeTag::IMMEDIATE,
+            elements: vec![packet.clone()],
+        };
+
+        assert_eq!(bundle.timetag, TimeTag::IMMEDIATE);
+        assert_eq!(bundle.elements.len(), 1);
+        assert_eq!(bundle.elements[0], packet);
+        if let OscPacket::Message(message) = &bundle.elements[0] {
+            assert_eq!(message, &inner_message);
+        } else {
+            panic!("expected message packet");
+        }
+    }
+
+    #[cfg(feature = "osc-1-1")]
+    #[test]
+    fn osc_1_1_specific_types_are_supported() {
+        let array_arg = OscType::Array(vec![
+            OscType::Int64(i64::MAX),
+            OscType::Double(core::f64::consts::PI),
+            OscType::True,
+            OscType::False,
+            OscType::Nil,
+            OscType::Infinitum,
+            OscType::Midi(0x1234_5678),
+            OscType::Color(0xff00_00ff),
+            OscType::Char('a'),
+        ]);
+
+        let message = OscMessage {
+            addr: Address("/extended".into()),
+            args: vec![
+                OscType::TimeTag(TimeTag(10)),
+                array_arg.clone(),
+            ],
+        };
+
+        assert_eq!(message.args.len(), 2);
+        assert_eq!(message.args[0], OscType::TimeTag(TimeTag(10)));
+        assert_eq!(message.args[1], array_arg);
+    }
+}
